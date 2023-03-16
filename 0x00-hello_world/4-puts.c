5204ebfc4082943101c53b869e184f5248848e3a
nougash1 <najlaataib@gmail.com>
2023-03-16 06:08:15 -0700

    task4. puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  main - starting point.
+ * Return: 0 if correct.
+ */
+int main(void)
+{
+	puts("\"Programming is like a building a multilingual puzzle");
+	return (0);
+}
