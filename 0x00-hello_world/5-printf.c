36e9063ffe1f88dd7b5c5db66e8729e41122d88d
nougash1 <najlaataib@gmail.com>
2023-03-16 09:37:34 -0700

    printf task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  main - starting point.
+ * Return: 0 if correct.
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
