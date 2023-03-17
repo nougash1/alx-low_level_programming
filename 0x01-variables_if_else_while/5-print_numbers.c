53ea23f1064df3c53173102914cf5b1bff792384
nougash1 <najlaataib@gmail.com>
2023-03-17 15:18:50 -0700

    numbers task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Display single digit numbers of base ten'
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;	
+	}
+	printf("\n");
+	return (0);
+}
