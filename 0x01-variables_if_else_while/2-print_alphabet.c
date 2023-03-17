02c0aaf02c48a6d4bc1b52be191af08da6e30fab
nougash1 <najlaataib@gmail.com>
2023-03-17 14:41:30 -0700

    display alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'display alphabets in lowercase usind putchar only'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 'a';
+	while (n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
