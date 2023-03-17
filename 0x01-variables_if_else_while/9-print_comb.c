9847a4af0087c59448ed4b93aa8b1b1edeb8bab1
nougash1 <najlaataib@gmail.com>
2023-03-17 16:47:23 -0700

    combinations of single digits numbers task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'using putchar onlu faour times'
+ * Return: 0
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		++i;
+	}
+	putchar('\n');
+	return (0);
+}
