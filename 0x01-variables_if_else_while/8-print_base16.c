d45e771a6eeb092f5e8f1560143aa73bc7446bc4
nougash1 <najlaataib@gmail.com>
2023-03-17 16:36:12 -0700

    hexadecimal task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Display numbers of base sixeteen using only putchar'
+ * Return: always 0
+ */
+int main(void)
+{
+	int num;
+	char l;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) +'0');
+	for (l = 'a'; l <= 'f'; l++)
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
