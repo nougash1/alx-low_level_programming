3031575d6ca2ad61114843d1883d6a4c3d0e52b0
nougash1 <najlaataib@gmail.com>
2023-03-17 15:59:14 -0700

    alphabets in reverse task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Display alphabets in reverse and lowercase using putchar'
+ * Return: 0
+ */
+int main(void)
+{
+        char l;
+
+        for (l = 'z'; l >= 'a'; l--)
+                putchar(l);
+        putchar('\n');
+        return (0);
+}
